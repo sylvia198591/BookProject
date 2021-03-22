3ad4a98dc812aaade8041dbdd32053815cc003cf
sylvia198591 <63388493+sylvia198591@users.noreply.github.com>
2021-03-22 13:48:42 +0530

    Update views.py

--- Book/views.py
+++ Book/views.py
@@ -60,9 +60,9 @@ class BookCreateView(ListCreateAPIView):
     authentication_classes = [TokenAuthentication,SessionAuthentication]
     permission_classes = [IsAdminUser]
 
-#     def get_queryset(self):
-#         # last_two_days = now() - timedelta(days=2)
-#         return Book.objects.all()
+    def get_queryset(self):
+        # last_two_days = now() - timedelta(days=2)
+        return Book.objects.all()
 #     def get_serializer_class(self):
 #         if self.request.method == 'GET':
 #             return BookSerializer1
